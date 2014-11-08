8d80cb3bdafd14d8bdb587dd0bd14809643d2b3f
Antti Minkkinen <g7786@student.jamk.fi>
2014-11-08 23:08:33 +0200

    files for netbeans

--- project/src/fi/jamk/productlister/ProductSearch.java
+++ project/src/fi/jamk/productlister/ProductSearch.java
@@ -15,29 +15,30 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class ProductSearch extends Activity {
+
 	private DBConnector db;
 	private ArrayList<Product> productlist;
 	ListView listViewProducts;
-	
+
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.product_search);
-        db = new DBConnector();
-        productlist = new ArrayList<Product>();
-        listViewProducts = (ListView)findViewById(R.id.product_search_listview);
-        
-        Button searchButton = (Button)findViewById(R.id.product_search_search);
-        
-        searchButton.setOnClickListener(new OnClickListener() {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.product_search);
+		db = new DBConnector();
+		productlist = new ArrayList<Product>();
+		listViewProducts = (ListView) findViewById(R.id.product_search_listview);
+
+		Button searchButton = (Button) findViewById(R.id.product_search_search);
+
+		searchButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				EditText textField = (EditText) findViewById(R.id.product_search_textfield);
 				String keyword = textField.getText().toString();
-				
-				SearchProducts search =  new SearchProducts();
+
+				SearchProducts search = new SearchProducts();
 				search.execute(keyword);
-				
+
 				try {
 					productlist = search.get();
 				} catch (InterruptedException e) {
@@ -47,35 +48,32 @@ public class ProductSearch extends Activity {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				ArrayAdapter<Product> newadapter = new ArrayAdapter<Product>(ProductSearch.this, android.R.layout.simple_list_item_1, productlist);
+				ArrayAdapter<Product> newadapter = new ArrayAdapter<Product>(ProductSearch.this,
+						android.R.layout.simple_list_item_1, productlist);
 				listViewProducts.setAdapter(newadapter);
-				
+
 			}
 		});
-        
-        
-        
-    }
-	
-	private class SearchProducts extends AsyncTask<String, Void, ArrayList<Product>>{
+
+	}
+
+	private class SearchProducts extends AsyncTask<String, Void, ArrayList<Product>> {
+
 		@Override
-		protected void onPreExecute(){
+		protected void onPreExecute() {
 			// TODO some other progress dialog?
 			Toast.makeText(getApplicationContext(), "Searching...", Toast.LENGTH_SHORT).show();
 		}
-		
+
 		@Override
 		protected ArrayList<Product> doInBackground(String... keyword) {
-			ArrayList<Product> list = new ArrayList<Product>();
-			list = db.searchProducts(keyword[0]);
-
-			return list;
+			return db.searchProducts(keyword[0]);
 		}
-		
+
 		@Override
-		protected void onPostExecute(ArrayList<Product> list){
+		protected void onPostExecute(ArrayList<Product> list) {
 			// TODO stop displaying other progress dialog
-			Toast.makeText(getApplicationContext(), "Found "+list.size()+" products.", Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(), "Found " + list.size() + " products.", Toast.LENGTH_SHORT).show();
 		}
 	}
 }
