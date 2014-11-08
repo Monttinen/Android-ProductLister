083d841f22c82255efb974359b215030cec65c8d
Antti Minkkinen <g7786@student.jamk.fi>
2014-11-08 20:01:06 +0200

    First version of product search working.

--- project/src/fi/jamk/productlister/DBConnector.java
+++ project/src/fi/jamk/productlister/DBConnector.java
@@ -1,26 +1,77 @@
 package fi.jamk.productlister;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
+
+import org.json.*;
+
 public class DBConnector {
+	private String server;
 	
 	public DBConnector(){
-		
+		server = "http://128.199.60.131/";
 	}
 	
-	public ArrayList<Product> searchProducts(String keyword){
+	public ArrayList<Product> searchProducts(String keyword) throws IOException, JSONException{
 		ArrayList<Product> results = new ArrayList<Product>();
-		if(keyword.length()<3){
+		if(keyword.length()<1){
 			return results;
 		}
 		
+		String responseString;
+		
+		// Get data string
+		String url = server + "searchproduct.php?keyword=" + keyword;
+	    responseString = getPage(url);
+	    
+	    // parse json and return arraylist
+		JSONObject json = new JSONObject(responseString);
+		if(json.getBoolean("Success")==true){
+			JSONArray products = json.getJSONArray("Products");
+			for(int i=0; i< products.length(); i++){
+				JSONObject p = products.getJSONObject(i);
+				results.add(new Product(p.getInt("idProduct"),
+										p.getInt("ProductCategoryId"),
+										p.getString("ProductName"),
+										p.getString("ProductBarcode")
+						));
+			}
+		}
 		
 		// dummy data:
-		results.add(new Product(1, 1, "testiolut", "bar1"));
+		/*results.add(new Product(1, 1, "testiolut", "bar1"));
 		results.add(new Product(2, 1, "testiolut2", "bar2"));
 		results.add(new Product(3, 1, "testiolut3", "bar3"));
-		results.add(new Product(4, 1, "testiolut4", "bar4"));
+		results.add(new Product(4, 1, "testiolut4", "bar4"));*/
 		
 		return results;
+	}
+	
+	private String getPage(String url) throws MalformedURLException, IOException {
+	    HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+	    con.connect();
+
+	    if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+	        return inputStreamToString(con.getInputStream());
+	    } else {
+	        return null;
+	    }
+	}
+
+	private String inputStreamToString(InputStream in) throws IOException {
+	    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+	    StringBuilder stringBuilder = new StringBuilder();
+	    String line = null;
+
+	    while ((line = bufferedReader.readLine()) != null) {
+	        stringBuilder.append(line + "\n");
+	    }
+
+	    bufferedReader.close();
+	    return stringBuilder.toString();
 	}
 }
--- project/src/fi/jamk/productlister/ProductSearch.java
+++ project/src/fi/jamk/productlister/ProductSearch.java
@@ -1,24 +1,93 @@
 package fi.jamk.productlister;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ProductSearch extends Activity {
 	private DBConnector db;
+	private ArrayList<Product> productlist;
+	ListView listViewProducts;
+	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_search);
         db = new DBConnector();
-        ArrayList<Product> test = db.searchProducts("olut");
+        productlist = new ArrayList<Product>();
+        listViewProducts = (ListView)findViewById(R.id.product_search_listview);
         
-        ArrayAdapter<Product> adapter = new ArrayAdapter<Product>(this, android.R.layout.simple_list_item_1, test);
-        ListView list = (ListView)findViewById(R.id.product_search_listview);
+        Button searchButton = (Button)findViewById(R.id.product_search_search);
         
-        list.setAdapter(adapter);
+        searchButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText textField = (EditText) findViewById(R.id.product_search_textfield);
+				String keyword = textField.getText().toString();
+				
+				SearchProducts search =  new SearchProducts();
+				search.execute(keyword);
+				
+				try {
+					productlist = search.get();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ArrayAdapter<Product> newadapter = new ArrayAdapter<Product>(ProductSearch.this, android.R.layout.simple_list_item_1, productlist);
+				listViewProducts.setAdapter(newadapter);
+				
+			}
+		});
+        
+        
+        
     }
+	
+	private class SearchProducts extends AsyncTask<String, Void, ArrayList<Product>>{
+		@Override
+		protected void onPreExecute(){
+			Toast.makeText(getApplicationContext(), "Searching...", Toast.LENGTH_SHORT).show();
+		}
+		
+		@Override
+		protected ArrayList<Product> doInBackground(String... keyword) {
+			ArrayList<Product> list = new ArrayList<Product>();
+			try {
+				list = db.searchProducts(keyword[0]);
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return list;
+		}
+		
+		@Override
+		protected void onPostExecute(ArrayList<Product> list){
+			Toast.makeText(getApplicationContext(), "Found "+list.size()+" products.", Toast.LENGTH_SHORT).show();
+		}
+	}
 }
