206dcaabd8112f42ada7e53e30468a70378cade6
Antti Minkkinen <g7786@student.jamk.fi>
2014-11-09 11:08:47 +0200

    Fixed a Derp

--- project/src/fi/jamk/productlister/ProductAdd.java
+++ project/src/fi/jamk/productlister/ProductAdd.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
  *
  * @author Monttinen
  */
-class ProductAdd extends Activity {
+public class ProductAdd extends Activity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
