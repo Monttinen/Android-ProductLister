f7e32531a56ac4b6d992d99bebaaff761ae6dcdb
unknown <james.pearce@hotmail.com>
2014-11-21 11:25:39 +0200

    minor work on addShop

--- project/src/fi/jamk/productlister/ShopAdd.java
+++ project/src/fi/jamk/productlister/ShopAdd.java
@@ -1,14 +1,11 @@
 package fi.jamk.productlister;
 
-import java.util.concurrent.ExecutionException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  *
@@ -16,6 +13,15 @@ import android.widget.Button;
  */
 public class ShopAdd extends Activity implements View.OnClickListener {
 
+	
+	
+	private EditText txtShopName;
+	private EditText txtShopAddress;
+	private EditText txtShopLocation;
+	
+	String ShopName;
+	String ShopAddress;
+	String ShopLocation;
 
 
 	@Override
@@ -25,22 +31,36 @@ public class ShopAdd extends Activity implements View.OnClickListener {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		((Button) findViewById(R.id.shop_add_addbutton)).setOnClickListener(this);
-
+		name = (EditText)findViewById(R.id.name);  
 		
 		
 
 
 }
 
+	
+	private void addShop() {
+		
+		
+		
+		Shop potato = new shop()
+		
+
+	}
+	
 
 	@Override
 	public void onClick(View v) {
-	//case R.id.shop_add_addbutton:
-		//addShop();
-	//	break;
-		
+		switch (v.getId()) {
+			case R.id.shop_add_addbutton:
+				addShop();
+				break;
+			
+
+		}
+
 	}
 
 
-
 }
+
